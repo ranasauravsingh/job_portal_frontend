73588c5f1df3f793977310b5487ce8b4a71f736a
ranasauravsingh <ranasaurav121@gmail.com>
2024-10-20 14:57:51 +0530

    [Configure][Global] react redux & redux persist integrated in main global file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+import "./index.css";
+import store from "./redux/store.ts";
+
+const persistor = persistStore(store);
+
+createRoot(document.getElementById("root")!).render(
+	// <StrictMode>
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>
+	// </StrictMode>
+);
